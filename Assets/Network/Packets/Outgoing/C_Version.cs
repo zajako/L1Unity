ef09b03870ba65352df95d9c9251e1bb623ca8e1
zajako <zajako@gmail.com>
2015-06-03 23:55:49 -0400

    Progress on the Client Packet format for outgoing packets. C_Version should be ready for use other than the client version needing to be changed to the appropriate version number for the server being tested with. I used the version for 3.80 just to fill it in for now.

--- Assets/Network/Packets/Outgoing/C_Version.cs
+++ Assets/Network/Packets/Outgoing/C_Version.cs
@@ -3,5 +3,21 @@ using System.Collections;
 
 public class C_Version : ClientPacketBase {
 
+	new void Start()
+	{
+		base.Start();
 
+		int clientLanguage = 0;				//Country: 0.US 3.Taiwan 4.Janpan 5.China
+		int clientVersion = 0x07cbf4dd;		
+		int serverType = 0x087f7dc2; 
+		int npcVersion = 0x07cbf4d9;
+
+		writeH(0);
+		writeC(0);
+		writeD(clientLanguage);
+		writeH(serverType);
+		writeH(npcVersion);
+		writeD(clientVersion);
+
+	}
 }
--- Assets/Network/Packets/Outgoing/ClientPacketBase.cs
+++ Assets/Network/Packets/Outgoing/ClientPacketBase.cs
@@ -1,7 +1,133 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Text;
+using System.IO;
 
 public class ClientPacketBase : MonoBehaviour {
 
+	MemoryStream _byteStream = new MemoryStream();
+	StringBuilder _packetString = new StringBuilder();
 
+	protected void Start()
+	{
+		_packetString.Append("Sending Packet: "+getType()+" ");
+
+	}
+
+	protected void writeD(int value)
+	{
+		addToStream((byte)(value & 0xFF));
+		addToStream((byte)((value >> 8) & 0xFF));
+		addToStream((byte)((value >> 16) & 0xFF));
+		addToStream((byte)((value >> 24) & 0xFF));
+
+		_packetString.Append("[D: "+value+"]");
+	}
+
+	protected void writeH(int value)
+	{
+		addToStream((byte)(value & 0xFF));
+		addToStream((byte)((value >> 8) & 0xFF));
+
+		_packetString.Append("[H: "+value+"]");
+	}
+
+	protected void writeC(int value)
+	{
+		addToStream((byte)(value & 0xFF));
+
+		_packetString.Append("[C: "+value+"]");
+	}
+
+	protected void writeP(int value)
+	{
+		addToStream((byte)(value));
+
+		_packetString.Append("[P: "+value+"]");
+	}
+
+	protected void writeL(long value)
+	{
+		addToStream((byte)(value & 0xFF));
+
+		_packetString.Append("[L: "+value+"]");
+	}
+
+	protected void writeF(double value)
+	{
+		// addToStream((byte)(value & 0xFF));
+		// addToStream((byte)((value >> 8) & 0xFF));
+		// addToStream((byte)((value >> 16) & 0xFF));
+		// addToStream((byte)((value >> 24) & 0xFF));
+		// addToStream((byte)((value >> 32) & 0xFF));
+		// addToStream((byte)((value >> 40) & 0xFF));
+		// addToStream((byte)((value >> 48) & 0xFF));
+		// addToStream((byte)((value >> 56) & 0xFF));
+
+		_packetString.Append("[L: "+value+"]");
+	}
+
+	protected void writeS(string value)
+	{
+		if(value != null)
+		{
+			addToStream(System.Text.Encoding.UTF8.GetBytes(value));
+		}
+
+		_packetString.Append("[S: "+value+"]");
+	}
+
+	protected void writeBytes(byte[] value)
+	{
+		if(value != null)
+		{
+			addToStream(value);
+		}
+		
+		_packetString.Append("[Byte: "+value+"]");
+	}
+
+	public int getLength()
+	{
+		return (int)(_byteStream.Length + 2);
+	}
+
+	public byte[] getBytes()
+	{
+		int padding = (int)_byteStream.Length % 4;
+		
+		if(padding != 0)
+		{
+			for(int i = padding; i < 4; i++)
+			{
+				writeC(0x00);
+			}
+		}
+		
+		return _byteStream.ToArray();
+	}
+
+
+	public string getType()
+	{
+		return "[C] " + ((object) this).GetType().Name;
+	}
+
+	public void printPacket()
+	{
+		Debug.Log(_packetString.ToString());
+	}
+
+	private void addToStream(byte b)
+	{
+		_byteStream.WriteByte(b);
+	}
+
+	private void addToStream(byte[] b)
+	{
+		for(int i = 0; i < b.Length; i++)
+		{
+			_byteStream.WriteByte(b[i]);
+		}
+	}
 }
