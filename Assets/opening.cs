467694529fec1ccf2e7b77de51ff3872a2442b20
zajako <zajako@gmail.com>
2015-06-19 20:59:52 -0400

    Login Fields now allow Tab, Shift-Tab and Enter to use them as would be expected. Rather than having to click through them and click the login button.

--- Assets/opening.cs
+++ Assets/opening.cs
@@ -1,20 +1,61 @@
 ﻿using UnityEngine;
-using UnityEngine.Events;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class opening : MonoBehaviour {
 
-
+	EventSystem system;
 	
 	// Use this for initialization
 	void Start () {
-	Debug.Log("Startup screen");
+		Debug.Log("Startup screen");
+		system = EventSystem.current;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	public void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Tab))
+        {
+			
+        	Selectable next = null;
+			if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+			{
+				next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnUp();
+				if(next==null)
+					next = system.lastSelectedGameObject.GetComponent<Selectable>();
+			}
+			else
+			{
+				next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+				if(next==null)
+					next = system.firstSelectedGameObject.GetComponent<Selectable>();
+			}
+			if (next != null)
+			{
+				InputField inputfield = next.GetComponent<InputField>();
+				if(inputfield != null)
+					inputfield.OnPointerClick(new PointerEventData(system));  //if it's an input field, also set the text caret
+				system.SetSelectedGameObject(next.gameObject, new BaseEventData(system));
+			}
+		}
+	}
+
+	public void OnGUI()
+	{
+		InputField login = GameObject.Find("Login").GetComponent<InputField>();
+		InputField password = GameObject.Find("Password").GetComponent<InputField>();
+
+		if(login.isFocused && login.text != "" && Input.GetKey(KeyCode.Return))
+		{
+ 			something();
+		}
+
+		if(password.isFocused && password.text != "" && Input.GetKey(KeyCode.Return))
+		{
+ 			something();
+		}
 	}
 	
 	public void something()
